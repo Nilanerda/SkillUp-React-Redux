63157e8f126789a56f756b85f2a87b51a4579281
Yana Mysyk <zzizzu97@gmail.com>
2019-03-17 15:00:09 +0200

    Updates

---
+++ precache-manifest.d376194000021ed8941eab8b4f90a6ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d53d97bb11e5a9b3b6014b0f1d6e46e",
+    "url": "/Nilanerda/SkillUp-React-Redux/static/media/heartActive.9d53d97b.svg"
+  },
+  {
+    "revision": "12aa4dc66b33df416cd41b95c3597c30",
+    "url": "/Nilanerda/SkillUp-React-Redux/static/media/heart.12aa4dc6.svg"
+  },
+  {
+    "revision": "9a5759813f894671c387",
+    "url": "/Nilanerda/SkillUp-React-Redux/static/js/runtime~main.9a575981.js"
+  },
+  {
+    "revision": "0c6cf8db8b47d04f752f",
+    "url": "/Nilanerda/SkillUp-React-Redux/static/js/main.0c6cf8db.chunk.js"
+  },
+  {
+    "revision": "5f5c9456699028d60061",
+    "url": "/Nilanerda/SkillUp-React-Redux/static/js/1.5f5c9456.chunk.js"
+  },
+  {
+    "revision": "0c6cf8db8b47d04f752f",
+    "url": "/Nilanerda/SkillUp-React-Redux/static/css/main.75b5381e.chunk.css"
+  },
+  {
+    "revision": "9060a2e43eec80408eb46fdf08ad5183",
+    "url": "/Nilanerda/SkillUp-React-Redux/index.html"
+  }
+];
